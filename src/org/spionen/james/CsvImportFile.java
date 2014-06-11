31dd77bbd30cf62f2b52ecb510c76c6f51524ed4
Tobias Olausson <tobsan@gmail.com>
2014-06-11 23:10:42 +0200

    Added stubs for classes to import/export from csv and excel

---
+++ src/org/spionen/james/CsvImportFile.java
@@ -0,0 +1,22 @@
+package org.spionen.james;
+
+import java.io.File;
+import java.util.ArrayList;
+
+// Use supercsv to read/write to these kinds of files
+
+public class CsvImportFile extends ImportFile {
+
+	@Override
+	public ArrayList<Prenumerant> readFile(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeFile(ArrayList<Prenumerant> subscribers, File file) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/spionen/james/ExcelImportFile.java
@@ -0,0 +1,22 @@
+package org.spionen.james;
+
+import java.io.File;
+import java.util.ArrayList;
+
+// TODO: Use Apache POI to read/write excel files
+
+public class ExcelImportFile extends ImportFile {
+
+	@Override
+	public ArrayList<Prenumerant> readFile(File file) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeFile(ArrayList<Prenumerant> subscribers, File file) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
