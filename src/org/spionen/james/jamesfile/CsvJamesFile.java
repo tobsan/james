198f0ef8003a35452ee953f84a4400b0a5812db9
Tobias Olausson <tobsan@tobsan.se>
2015-01-15 21:11:13 +0100

    Added license header to all source code files. Updated TODO slightly.

--- src/org/spionen/james/jamesfile/CsvJamesFile.java
+++ src/org/spionen/james/jamesfile/CsvJamesFile.java
@@ -1,3 +1,21 @@
+/**
+*   Copyright 2010-2015 Maxim Fris, Tobias Olausson
+*
+*   This file is part of James.
+*
+*   James is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   James is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with James. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.spionen.james.jamesfile;
 
 import org.spionen.james.subscriber.Subscriber;
